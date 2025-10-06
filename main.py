4fd3310dd8c31042e75d53ecde882df5777fcf31
MarioSquirt <kyle.a.t@hotmail.co.uk>
2025-10-06 19:12:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import sys
 import os
 
 def open_benefit_calculator():
-    script_path = os.path.join(os.path.dirname(__file__), 'Benefit_calculator.py')
+    script_path = os.path.join(os.path.dirname(__file__), 'benefit_calculator.py')
     if not os.path.isfile(script_path):
-        print("Benefit_calculator.py not found.")
+        print("benefit_calculator.py not found.")
         return
     subprocess.run([sys.executable, script_path])
 
 if __name__ == "__main__":
+
     open_benefit_calculator()
